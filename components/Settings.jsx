662eecf83643f396d68dee31be9d3ea786479b58
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-03-31 00:59:35 -0700

    Update Settings.jsx

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -14,37 +14,14 @@ module.exports = class Settings extends React.PureComponent {
 		const { getSetting, updateSetting, toggleSetting } = this.props;
 		return (
 			<div id="F8settings">
-				<div class="Bubfeat chan-name">
-					<p>F8 Settings</p>
-				</div>
-				<div class="upcoming">
-					<strong> Features coming in the future </strong>
-					<div>
-						<p> Make the two buttons work </p>
-					</div>
-				</div>
-				<RadioGroup
-					onChange={e => {
-						updateSetting('F8fixkey', e.value);
-						if (e.value === 1) updateSetting('indicator', false);
+				<SwitchItem
+					value={getSetting('CustomKeybind')}
+					onChange={() => {
+						toggleSetting('CustomKeybind')
 					}}
-					value={getSetting('F8fixkey', 0)}
-					options={[
-						{
-							name: 'F8',
-							value: 0
-						},
-						{
-							name: 'Custom Keybind',
-							value: 1
-						}
-					]}
 				>
-					<div>
-						F8 fix key
-						<p class="warning">Dont do repeat your keybind it may freeze discord</p>
-					</div>
-				</RadioGroup>
+					Custom Keybind
+        		</SwitchItem>
 					<KeybindRecorder value={getSetting('keybind', '6')}
 						onChange={(e) => {
 							this.setState({ value: e })
