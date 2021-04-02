5d56fdb715b9ee579c2f2aaf46870405543d15cf
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-04-02 16:03:02 -0700

    Adds the F10 WARNING

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -12,11 +12,14 @@ module.exports = class Settings extends React.PureComponent {
 		const { getSetting, updateSetting, toggleSetting } = this.props;
 		return (
 			<div id="F8settings">
-					<KeybindRecorder value={getSetting('keybind', '8')}
-						onChange={(e) => {
-							this.setState({ value: e })
-							updateSetting('keybind', e)
-						}}
+				<div id="desc">
+					<p>Warning </p> <strong>F10</strong> <p> causes muliple bugs</p>
+				</div>
+				<KeybindRecorder value={getSetting('keybind', '8')}
+					onChange={(e) => {
+						this.setState({ value: e })
+						updateSetting('keybind', e)
+					}}
 					onReset={() => {
 							this.setState({ value: 'F8' })
 							updateSetting('keybind', 'F8')
